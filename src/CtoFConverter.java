570965fa638a25b58b3c17c92145ccbe4baa8f79
James910066 <95539722+James910066@users.noreply.github.com>
2023-02-24 18:36:27 -0500

    Update CtoFConverter.java

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,35 +1,45 @@
 import java.util.Scanner; //Import of Scanner
 public class CtoFConverter
 {
-    public static void main(String[] args)
+    public static void main (String[]args)
     {
-        double tempInCelsius = 0; //Variable declaration and assignment
-        double tempInFarhenheit = 0; //Variable declaration and assignment
-        String trash = ""; //Variable declaration and assignment
-        final int FREEZINGTEMP = 0; //Declaration and assignment of freezing point in celsius
-        final int BOILINGTEMP = 100; //Declaration and assignment of boiling point in celsius
+        //Declaration and assignment of all variables
+        int tempInCelsius = 0;
+        int tempInFarhenheit = 0;
+        String trash = "";
+        final int FREEZINGTEMP = 0;
+        final int BOILINGTEMP = 100;
+        boolean correctInput = false;
+        Scanner in = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in); //Declaration and assignment of Scanner utility
-
-        System.out.println("Enter your temperature choice for Celsius"); //Prompt user for input
-        if(in.hasNextDouble()) //Check and validate user input
+        do //Start of Do While loop for testing user input
         {
-            tempInCelsius = in.nextDouble(); //Assigning user input to variable
-            if(tempInCelsius == FREEZINGTEMP) //Check input for freezing point
-            {
-                System.out.println("You entered temperature for freezing point: " + FREEZINGTEMP); //Output statement
-            }
-            if(tempInCelsius == BOILINGTEMP) //Check input for boiling point
-            {
-                System.out.println("You entered temperature for boiling point in celsius " + BOILINGTEMP); //Output statement
-            }
-            tempInFarhenheit = tempInCelsius * 9 / 5 + 32; //Calculation process
-            System.out.println("Your temperature converted to fahrenheit is " + tempInFarhenheit); //Output statement
+            System.out.println("Enter your temperature choice for Celsius"); //Prompt user for input
+            if (in.hasNextInt()) //Check if user input is an integer and not string
+                {
+                    tempInCelsius = in.nextInt(); //Assignment of correct user input
+                    correctInput = true; //Reassign variable condition to end loop
+                }
+            else //When the check for integer fails, user input goes through here
+                {
+                    trash = in.nextLine(); //Assignment of bad input from user
+                    System.out.println(trash + " is invalid input."); //Output statement of bad input to user
+                }
         }
-        else //Exit on bad input
+        while (!correctInput); //Check to see if condition is met to continue loop for user input
         {
-            trash = in.nextLine(); //Assignment of bad input from user
-            System.out.println("You entered an invalid number for temperature: " + trash); //Output statement
+            {
+                if(tempInCelsius == FREEZINGTEMP) //Check input for freezing point
+                    {
+                        System.out.println("You entered temperature for freezing point: " + FREEZINGTEMP); //Output statement
+                    }
+                if(tempInCelsius == BOILINGTEMP) //Check input for boiling point
+                    {
+                        System.out.println("You entered temperature for boiling point in celsius " + BOILINGTEMP); //Output statement
+                    }
+                tempInFarhenheit = tempInCelsius * 9 / 5 + 32; //Calculation process
+                System.out.println("Your temperature converted to fahrenheit is " + tempInFarhenheit); //Output statement
+            }
         }
     }
 }
